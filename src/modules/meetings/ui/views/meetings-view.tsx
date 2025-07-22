cc645530cd4e74d45caa972371ba1d0ea594f4e7
Joe Kariuki <jknguru6@gmail.com>
2025-07-22 08:53:28 +0300

    feat(meetings): add MeetingsView component
    - Create MeetingsView client component
    - Fetch meetings list using trpc.meetings.getMany with React Query
    - Render JSON.stringify(data) placeholder for debugging

---
+++ src/modules/meetings/ui/views/meetings-view.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
+
+export function MeetingsView() {
+  const trpc = useTRPC();
+  const { data } = useQuery(trpc.meetings.getMany.queryOptions({}));
+
+  return <div>{JSON.stringify(data)}</div>;
+}
