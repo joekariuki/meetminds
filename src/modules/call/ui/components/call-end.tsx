f0c2d96facc622797f5093562fa0b8c5f8f798da
Joe Kariuki <jknguru6@gmail.com>
2025-07-29 12:06:08 +0300

    feat(calls): add CallEnded component for post-call UI
    - Render “You have ended the call” screen with summary placeholder
    - Center content in radial‑gradient background and styled container
    - Include back button using ArrowLeftIcon and Next.js Link to /meetings

---
+++ src/modules/call/ui/components/call-end.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+
+import "@stream-io/video-react-sdk/dist/css/styles.css";
+import { ArrowLeftIcon } from "lucide-react";
+
+export function CallEnded() {
+  return (
+    <div className="flex flex-col items-center justify-center h-full bg-radial from-sidebar-accent to-sidebar">
+      <div className="py-4 px-8 flex flex-1 items-center justify-center">
+        <div className="flex flex-col items-center justify-center gap-y-6 bg-background rounded-lg p-10 shadow-sm">
+          <div className="flex flex-col gap-y-2 text-center">
+            <h6 className="text-lg font-medium">You have ended the call</h6>
+            <p className="text-sm">Summary will apppear in a few minutes</p>
+          </div>
+
+          <Button>
+            <ArrowLeftIcon />
+            <Link href="/meetings">Back to meetings</Link>
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
